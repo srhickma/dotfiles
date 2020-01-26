1a3c66be0340b323ea6397f86cf0de20cc7e72a0
Shane Hickman <srhickma@edu.uwaterloo.ca>
2020-01-26 04:55:22 -0500

    Move package configuration into packages.el

--- .layers/x-wide/config.el
+++ .layers/x-wide/config.el
@@ -6,23 +6,3 @@
 
 ;; use the clipboard for cut and paste
 (setq select-enable-clipboard t)
-
-;; keep cursor centered in modes other than info, term, shell, ...
-(require 'centered-cursor-mode)
-(define-global-minor-mode filter-global-centered-cursor-mode centered-cursor-mode
-  (lambda ()
-    (when (not (memq major-mode
-                     (list 'Info-mode 'term-mode 'eshell-mode 'shell-mode 'erc-mode)))
-      (centered-cursor-mode))))
-(filter-global-centered-cursor-mode 1)
-
-;; auto-complete overrides to combat spacemacs initialization.
-(use-package auto-complete
-  :config
-  (setq ac-auto-start 2
-        ac-delay 0.2
-        tab-always-indent t))
-
-(use-package avy
-  :bind ("C-:" . avy-goto-char)
-  :config (setq avy-all-windows nil))
